346c23da32006fa3515e0a31d7e38714d02a9b91
Alexander Jenkins <22840490+alexander-jenkins@users.noreply.github.com>
2022-05-01 20:58:20 -0400

    added changes during meeting for component examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TitleBarComponent } from './title-bar/title-bar.component';
+import { ActionButtonComponent } from './action-button/action-button.component';
+import { StudentHomeComponent } from './student-home/student-home.component';
+import { InstructorHomeComponent } from './instructor-home/instructor-home.component';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavbarComponent, TitleBarComponent, ActionButtonComponent, StudentHomeComponent, InstructorHomeComponent, AdminHomeComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
