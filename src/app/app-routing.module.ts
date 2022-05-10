25d08f47522fc4a53a04fb6fde86d6f36c9bb6b2
Alexander Jenkins <22840490+alexander-jenkins@users.noreply.github.com>
2022-05-10 09:13:07 -0400

    condensed homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,39 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminHomeComponent } from './admin-home/admin-home.component';
 import { CourseStatusesComponent } from './course-statuses/course-statuses.component';
 import { EnrollClassComponent } from './enroll-class/enroll-class.component';
-import { InstructorHomeComponent } from './instructor-home/instructor-home.component';
+import { HomeComponent } from './home/home.component';
 import { ManageClassesComponent } from './manage-classes/manage-classes.component';
 import { ManageCoursesComponent } from './manage-courses/manage-courses.component';
 import { ManageSemesterComponent } from './manage-semester/manage-semester.component';
 import { ManageStudentsComponent } from './manage-students/manage-students.component';
 import { ScheduleComponent } from './schedule/schedule.component';
-import { StudentHomeComponent } from './student-home/student-home.component';
 import { ViewClassComponent } from './view-class/view-class.component';
 import { ViewGradesComponent } from './view-grades/view-grades.component';
 
 const routes: Routes = [
-  {path:'', redirectTo: 'studenthome', pathMatch: 'full'},
-  
-  {path:'adminhome', component:AdminHomeComponent},
-  {path:'managecourse', component:ManageCoursesComponent},
-  {path:'manageclasses', component:ManageClassesComponent},
-  {path:'managesemester', component:ManageSemesterComponent},
-  {path:'managestudents', component:ManageStudentsComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
 
-  {path:'instructorhome', component:InstructorHomeComponent},
-  {path:'viewclass', component:ViewClassComponent},
+  { path: 'managecourse', component: ManageCoursesComponent },
+  { path: 'manageclasses', component: ManageClassesComponent },
+  { path: 'managesemester', component: ManageSemesterComponent },
+  { path: 'managestudents', component: ManageStudentsComponent },
 
-  {path:'studenthome', component:StudentHomeComponent},
-  {path:'schedule', component:ScheduleComponent},
-  {path:'enrollclass', component:EnrollClassComponent},
-  {path:'coursestatuses', component:CourseStatusesComponent},
-  {path:'viewgrades', component:ViewGradesComponent}
+  { path: 'viewclass', component: ViewClassComponent },
+
+  { path: 'home', component: HomeComponent },
+  { path: 'schedule', component: ScheduleComponent },
+  { path: 'enrollclass', component: EnrollClassComponent },
+  { path: 'coursestatuses', component: CourseStatusesComponent },
+  { path: 'viewgrades', component: ViewGradesComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,6 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TitleBarComponent } from './title-bar/title-bar.component';
 import { ActionButtonComponent } from './action-button/action-button.component';
-import { StudentHomeComponent } from './student-home/student-home.component';
-import { InstructorHomeComponent } from './instructor-home/instructor-home.component';
-import { AdminHomeComponent } from './admin-home/admin-home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ManageCoursesComponent } from './manage-courses/manage-courses.component';
 import { ManageClassesComponent } from './manage-classes/manage-classes.component';
@@ -19,12 +16,31 @@ import { ViewClassComponent } from './view-class/view-class.component';
 import { EnrollClassComponent } from './enroll-class/enroll-class.component';
 import { CourseStatusesComponent } from './course-statuses/course-statuses.component';
 import { ViewGradesComponent } from './view-grades/view-grades.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, TitleBarComponent, ActionButtonComponent, StudentHomeComponent, InstructorHomeComponent, AdminHomeComponent, ManageCoursesComponent, ManageClassesComponent, ManageSemesterComponent, ManageStudentsComponent, ViewClassComponent, EnrollClassComponent, CourseStatusesComponent, ViewGradesComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    TitleBarComponent,
+    ActionButtonComponent,
+    ManageCoursesComponent,
+    ManageClassesComponent,
+    ManageSemesterComponent,
+    ManageStudentsComponent,
+    ViewClassComponent,
+    EnrollClassComponent,
+    CourseStatusesComponent,
+    ViewGradesComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
-
